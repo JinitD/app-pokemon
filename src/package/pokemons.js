e2bfb9a95886c5113db9bceeb89256f8a0eb4016
Jhoan Dorado <Jhoan>
2023-04-23 18:30:20 -0500

    uso de axios

--- src/package/pokemons.js
+++ src/package/pokemons.js
@@ -1,17 +1,16 @@
 
 const API = {}
-
+const axios = require("axios");
 const URLPOKEMON = 'https://pokeapi.co/api/v2/pokemon/'
-
 API.pokemons = async () => {
     const pokemons = [];
-    const response = await fetch(`${URLPOKEMON}`);
-    const jsonData = await response.json();
+    const response = await axios.get(`${URLPOKEMON}`);
+    const jsonData = await response.data
 
     for (const p of jsonData.results) {
         const url = p.url.replace(URLPOKEMON, '');
-        const pokemonResponse = await fetch(`${URLPOKEMON}${url}`);
-        const pokemonData = await pokemonResponse.json();
+        const pokemonResponse = await axios.get(`${URLPOKEMON}/${url}`);
+        const pokemonData = await pokemonResponse.data
         const ability = pokemonData.abilities[0].ability.name;
         const img = pokemonData.sprites.front_default;
         const baseExperience = pokemonData.base_experience;
@@ -21,8 +20,8 @@ API.pokemons = async () => {
     return pokemons;
 }
 API.pokemon = async (url) => {
-    const response = await fetch(URLPOKEMON + url);
-    const jsonData = await response.json();
+    const response = await axios.get(URLPOKEMON + url);
+    const jsonData = await response.data;
     imgurl = jsonData.sprites.versions["generation-v"]["black-white"].animated.front_default;
     baseExp = jsonData.base_experience
     names = jsonData.name
@@ -32,7 +31,7 @@ API.pokemon = async (url) => {
     defensa = jsonData.stats[2].base_stat
     attk = jsonData.stats[1].base_stat
     hp = jsonData.stats[0].base_stat
-    return {baseExp,names,speed,specialDefense,specialAtack,defensa,attk,hp,imgurl};
+    return { baseExp, names, speed, specialDefense, specialAtack, defensa, attk, hp, imgurl };
 }
 
 
